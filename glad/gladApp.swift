122ba98f501d023d79b0162af98b7fabaa56d933
bagaking <zfaustk@qq.com>
2024-09-28 01:35:32 +0800

    Initial Commit

---
+++ glad/gladApp.swift
@@ -0,0 +1,17 @@
+//
+//  gladApp.swift
+//  glad
+//
+//  Created by bytedance on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct gladApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gladTests/gladTests.swift
@@ -0,0 +1,17 @@
+//
+//  gladTests.swift
+//  gladTests
+//
+//  Created by bytedance on 9/28/24.
+//
+
+import Testing
+@testable import glad
+
+struct gladTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
